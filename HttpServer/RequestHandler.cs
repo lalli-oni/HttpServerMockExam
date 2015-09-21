0942bb6d549ceb1ada2d20799cdc0a76464bf35b
lalli-oni <lalli.oni@gmail.com>
2015-09-21 14:24:54 +0200

    Response is complying to HTTP standards. Working in browser.

---
+++ HttpServer/RequestHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    public class RequestHandler
+    {
+        private String _requestMessage;
+
+        public String RequesMessage
+        {
+            get { return _requestMessage; }
+            set { _requestMessage = value; }
+        }
+
+        /// <summary>
+        /// Contstructor, forces a TcpClient as a parameter.
+        /// </summary>
+        /// <param name="requestingClient"></param>
+        public RequestHandler(Socket requestingClient)
+        {
+            try
+            {
+                int msgLength = requestingClient.Available;
+                byte[] buffer = new byte[msgLength];
+                requestingClient.Receive(buffer);
+                _requestMessage = "";
+                _requestMessage = _requestMessage + Encoding.ASCII.GetString(buffer, 0, msgLength);
+                WriteRequest(_requestMessage);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine("Couldn't read request stream to end, exception:");
+                Trace.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Just writes out the incoming request in console
+        /// </summary>
+        /// <param name="request">The request being received</param>
+        public void WriteRequest(string request)
+        {
+            Console.WriteLine(request);
+        }
+    }
+}
---
+++ HttpServer/ResponseHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HttpServer
+{
+    public class ResponseHandler
+    {
+        private HttpResponseMessage _responseMessage;
+
+        public HttpResponseMessage ResponseMessage
+        {
+            get { return _responseMessage; }
+            set { _responseMessage = value; }
+        }
+
+        public ResponseHandler(string request, Socket client)
+        {
+            _responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            Byte[] bytesResponse = Encoding.ASCII.GetBytes("Hello world!");
+            string sBuffer = "";
+            sBuffer = sBuffer + "HTTP/1.1" + " 200 OK" + "\r\n";
+            sBuffer = sBuffer + "Server: cx1193719-b\r\n";
+            sBuffer = sBuffer + "Content-Type: " + "text/html" + "\r\n";
+            sBuffer = sBuffer + "Accept-Ranges: bytes\r\n";
+            sBuffer = sBuffer + "Content-Length: " + bytesResponse.Length + "\r\n\r\n";
+            sBuffer = sBuffer + "Hello world!\r\n";
+
+            if (request.Contains("GET"))
+            {
+                HttpContent cont = new StringContent("Hello world!");
+                _responseMessage.Content = cont;
+                Byte[] bSendData = Encoding.ASCII.GetBytes(sBuffer);
+                client.Send(bSendData);
+                client.Dispose();
+            }
+        }
+    }
+}
