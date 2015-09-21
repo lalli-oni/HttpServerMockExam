f0e571026ec6cbc5a620b54fc375e7b3fb18d599
lalli-oni <lalli.oni@gmail.com>
2015-09-21 10:11:00 +0200

    Initialized solution containing C# console project and Unit Test.

---
+++ HttpUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttpUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
